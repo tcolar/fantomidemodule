2db61eb66a5463d5eca7710862beb4001e9bf7e9
tcolar <devnull@localhost>
2009-08-15 05:39:07 +0000

    1.3.2

--- src/net/colar/netbeans/fan/platform/FanPlatform.java
+++ src/net/colar/netbeans/fan/platform/FanPlatform.java
@@ -14,67 +14,67 @@ import java.io.File;
 public class FanPlatform
 {
 
-    private final boolean IS_WIN = System.getProperty("os.name").toLowerCase().indexOf("windows") != -1;
-    private static FanPlatform instance = new FanPlatform();
-    private String fanHome;
-    private String fanBin;
-    private String fanshBin;
+	private final boolean IS_WIN = System.getProperty("os.name").toLowerCase().indexOf("windows") != -1;
+	private static FanPlatform instance = new FanPlatform();
+	private String fanHome;
+	private String fanBin;
+	private String fanshBin;
 
-    public FanPlatform()
-    {
-	readSettings();
-    }
-
-    public void readSettings()
-    {
-	fanHome = FanPlatformSettings.getInstance().get(FanPlatformSettings.PREF_FAN_HOME);
-	if (fanHome != null)
+	public FanPlatform()
 	{
-	    if (!fanHome.endsWith(File.separator))
-	    {
-		fanHome += File.separator;
-	    }
-	    fanBin = fanHome + "bin" + File.separator + (IS_WIN ? "fan.exe" : "fan");
-	    fanshBin = fanHome + "bin" + File.separator + (IS_WIN ? "fansh.exe" : "fansh");
+		readSettings();
 	}
-    }
 
-    public static boolean checkFanHome(String path)
-    {
-	if (path != null)
+	public void readSettings()
 	{
-	    File f = new File(path);
-	    if (f.exists() && f.isDirectory())
-	    {
-		File exe = new File(path + File.separator + "bin", "fan");
-		return exe.exists() && exe.isFile() && exe.canExecute();
-	    }
+		fanHome = FanPlatformSettings.getInstance().get(FanPlatformSettings.PREF_FAN_HOME);
+		if (fanHome != null)
+		{
+			if (!fanHome.endsWith(File.separator))
+			{
+				fanHome += File.separator;
+			}
+			fanBin = fanHome + "bin" + File.separator + (IS_WIN ? "fan.exe" : "fan");
+			fanshBin = fanHome + "bin" + File.separator + (IS_WIN ? "fansh.exe" : "fansh");
+		}
 	}
-	return false;
-    }
 
-    public static FanPlatform getInstance()
-    {
-	return getInstance(true);
-    }
-
-    public static FanPlatform getInstance(boolean checkNull)
-    {
-	if (checkNull && instance.fanHome == null)
+	public static boolean checkFanHome(String path)
 	{
-	    //TODO: handle this cleanly (warning dialog)
-	    throw new RuntimeException("Fan Home is undefined, update Netbeans options!");
+		if (path != null)
+		{
+			File f = new File(path);
+			if (f.exists() && f.isDirectory())
+			{
+				File exe = new File(path + File.separator + "bin", "fan");
+				return exe.exists() && exe.isFile() /*&& exe.canExecute()*/;//TODO: fix
+			}
+		}
+		return false;
 	}
-	return instance;
-    }
 
-    public String getFanBinaryPath()
-    {
-	return fanBin;
-    }
+	public static FanPlatform getInstance()
+	{
+		return getInstance(true);
+	}
 
-    public String getFanShellBinaryPath()
-    {
-	return fanshBin;
-    }
+	public static FanPlatform getInstance(boolean checkNull)
+	{
+		if (checkNull && instance.fanHome == null)
+		{
+			//TODO: handle this cleanly (warning dialog)
+			throw new RuntimeException("Fan Home is undefined, update Netbeans options!");
+		}
+		return instance;
+	}
+
+	public String getFanBinaryPath()
+	{
+		return fanBin;
+	}
+
+	public String getFanShellBinaryPath()
+	{
+		return fanshBin;
+	}
 }
