6c0c025db93f9bf97725792ea8eb45a92ec7fe46
tcolar <devnull@localhost>
2009-08-28 20:57:01 +0000

    debugger fixes

---
+++ test/Debug/java/me/newClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me;
+
+/**
+ *
+ * @author thibautc
+ */
+public class newClass {
+    static void main(String[] args)
+    {
+        System.out.println();
+    }
+}
