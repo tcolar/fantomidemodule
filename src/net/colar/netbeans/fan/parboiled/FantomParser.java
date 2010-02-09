0eaaadc903559706539d294e595ea3f9305859c9
tcolar <devnull@localhost>
2010-02-09 00:17:50 +0000

    Full parboiled grammar - not tested yet

--- src/net/colar/netbeans/fan/parboiled/FantomParser.java
+++ src/net/colar/netbeans/fan/parboiled/FantomParser.java
@@ -3,6 +3,11 @@
  */
 package net.colar.netbeans.fan.parboiled;
 
+import net.colar.netbeans.fan.antlr.FanParser.condAndExpr_return;
+import net.colar.netbeans.fan.antlr.FanParser.condOrExpr_return;
+import net.colar.netbeans.fan.antlr.FanParser.idExprReq_return;
+import net.colar.netbeans.fan.antlr.FanParser.mapPair_return;
+import net.colar.netbeans.fan.antlr.FanParser.unaryExpr_return;
 import org.parboiled.BaseParser;
 import org.parboiled.MatcherContext;
 import org.parboiled.Node;
@@ -26,6 +31,8 @@ import org.parboiled.support.ParseTreeUtils;
 public class FantomParser extends BaseParser<Object>
 {
 	// ------------ Comp Unit --------------------------------------------------
+
+	//TODO: look for all specila code in fan.g (comments) and see what needs to be brought over
 
 	public Rule compilationUnit()
 	{
@@ -227,7 +234,7 @@ public class FantomParser extends BaseParser<Object>
 	public Rule block()
 	{
 		return firstOf(
-				enforcedSequence(BRACKET_L, zeroOrMore(stmt()), BRACKET_R),
+				itBlock(), // it block is normal block
 				stmt() // singlr statement
 			);
 	}
@@ -294,8 +301,368 @@ public class FantomParser extends BaseParser<Object>
 	// ----------- Expressions -------------------------------------------------
 	public Rule expr()
 	{
-		//TODO: dummy
-		return oneOrMore("XXX");
+		return assignExpr();
+	}
+
+	public Rule assignExpr()
+	{
+		return sequence(ifExpr(), optional(enforcedSequence(firstOf(AS_EQUAL, AS_ASSIGN_OPS), assignExpr())));
+	}
+
+	public Rule ifExpr()
+	{
+		return firstOf(elvisExpr(), ternaryExpr());
+	}
+
+	public Rule ternaryExpr()
+	{
+		return sequence(condOrExpr(),
+			optional(enforcedSequence(SP_QMARK, ifExprBody(), SP_COL, ifExprBody())));
+	}
+
+	public Rule elvisExpr()
+	{
+		return sequence(condOrExpr(), OP_ELVIS, ifExprBody());
+	}
+
+	public Rule ifExprBody()
+	{
+		return firstOf(condOrExpr(), enforcedSequence(KW_THROW, expr()));
+	}
+
+	public Rule condOrExpr()
+	{
+		return sequence(condAndExpr(), zeroOrMore(enforcedSequence(OP_OR, condAndExpr())));
+	}
+
+	public Rule condAndExpr()
+	{
+		return sequence(equalityExpr(), zeroOrMore(enforcedSequence(OP_AND, equalityExpr())));
+	}
+
+	public Rule equalityExpr()
+	{
+		return sequence(relationalExpr(), zeroOrMore(enforcedSequence(CP_EQUALITY, relationalExpr())));
+	}
+
+	public Rule relationalExpr()
+	{
+		return firstOf(typeCheckExpr(), compareExpr());
+	}
+
+	public Rule typeCheckExpr()
+	{
+		return sequence(rangeExpr(),
+			optional(enforcedSequence(firstOf(KW_IS, KW_ISNOT, KW_AS),type())));
+	}
+
+	public Rule compareExpr()
+	{
+		return sequence(rangeExpr(), zeroOrMore(enforcedSequence(CP_COMPARATORS, rangeExpr())));
+	}
+
+	public Rule rangeExpr()
+	{
+		return sequence(addExpr(), 
+			zeroOrMore(enforcedSequence(firstOf(OP_RANGE_EXCL, OP_RANGE), addExpr())));
+	}
+
+	public Rule addExpr()
+	{
+		return sequence(multExpr(),
+			zeroOrMore(enforcedSequence(firstOf(OP_PLUS, OP_MINUS), multExpr())));
+	}
+
+	public Rule multExpr()
+	{
+		return sequence(parExpr(),
+			zeroOrMore(enforcedSequence(firstOf(OP_MULT, OP_DIV), parExpr())));
+	}
+
+	public Rule parExpr()
+	{
+		return firstOf(castExpr(), groupedExpr(), unaryExpr());
+	}
+
+	public Rule castExpr()
+	{
+		return sequence(noNl(), PAR_L, type(), PAR_R, noNl(), parExpr());
+	}
+
+	public Rule groupedExpr()
+	{
+		return sequence(PAR_L, expr(), PAR_R, zeroOrMore(termChain()));
+	}
+
+	public Rule unaryExpr()
+	{
+		return firstOf(prefixExpr(), postfixExpr(), termExpr());
+	}
+
+	public Rule prefixExpr()
+	{
+		return sequence(
+				firstOf(OP_CURRY, OP_BANG, OP_2PLUS, OP_2MINUS, OP_PLUS, OP_MINUS),
+				parExpr());
+	}
+
+	public Rule postfixExpr()
+	{
+		return sequence(termExpr(), firstOf(OP_2MINUS, OP_2PLUS));
+	}
+
+	public Rule termExpr()
+	{
+		return sequence(termBase(), zeroOrMore(termChain()));
+	}
+
+	public Rule termBase()
+	{
+		// check for ID alone last (and not as part of idExpr) otherwise it would never check literal & typebase !
+		return firstOf(idExprReq(), litteral(), typeBase(), id());
+	}
+
+	public Rule typeBase()
+	{
+		return firstOf(typeLitteral(), slotLitteral(), namedSuper(), staticCall(),
+						dsl(), closure(), simple(), ctorBlock());
+	}
+
+	public Rule typeLitteral()
+	{
+		return sequence(type(), noNl(), OP_POUND);
+	}
+
+	public Rule slotLitteral()
+	{
+		return sequence(optional(type()), noNl(), OP_POUND, noNl(), id());
+	}
+
+	public Rule namedSuper()
+	{
+		return sequence(type(), DOT, KW_SUPER);
+	}
+
+	public Rule staticCall()
+	{
+		return sequence(type(), DOT, idExpr());
+	}
+
+	public Rule dsl()
+	{
+		//TODO: unclosed DSL ?
+		return sequence(simpleType(),
+			sequence(DSL_OPEN, zeroOrMore(sequence(testNot(DSL_CLOSE), any())), DSL_OPEN));
+	}
+
+	public Rule closure()
+	{
+		return sequence(funcType(), BRACKET_L, zeroOrMore(stmt()), BRACKET_R);
+	}
+
+	public Rule simple()
+	{
+		return sequence(type(), PAR_L, expr(), PAR_R);
+	}
+
+	public Rule ctorBlock()
+	{
+		return sequence(type(), noNl(), itBlock());
+	}
+
+	public Rule itBlock()
+	{
+		return enforcedSequence(BRACKET_L, zeroOrMore(stmt()), BRACKET_R);
+	}
+
+	public Rule termChain()
+	{
+		return firstOf(safeDotCall(), safeDynCall(), dotCall(), dynCall(), indexExpr(),
+						callOp(), itBlock(), incDotCall());
+	}
+
+	public Rule dotCall()
+	{
+		return enforcedSequence(DOT, idExpr());
+	}
+
+	public Rule dynCall()
+	{
+		return enforcedSequence(OP_ARROW, idExpr());
+	}
+
+	public Rule safeDotCall()
+	{
+		return enforcedSequence(OP_SAFE_CALL, idExpr());
+	}
+
+	public Rule safeDynCall()
+	{
+		return enforcedSequence(OP_SAFE_DYN_CALL, idExpr());
+	}
+
+	// incomplete dot call, make valid to allow for completion
+	public Rule incDotCall()
+	{
+		return DOT; 
+	}
+
+	public Rule idExpr()
+	{
+		// this can be either a local def(toto.value) or a call(toto.getValue or toto.getValue(<params>)) + opt. closure
+		return firstOf(idExprReq(), id());
+	}
+
+	public Rule idExprReq()
+	{
+		// Same but without matching ID by itself (this would prevent termbase from checking literals).
+		return firstOf(field(), call());
+	}
+
+	public Rule field()
+	{
+		return sequence(optional(OP_MULT),id());
+	}
+
+	public Rule call()
+	{
+		return sequence(id(),
+						optional(enforcedSequence(PAR_L, args(), PAR_R)), // params
+						optional(closure()));
+	}
+
+	/*
+	 *  TODO: does not always  work, will be parsed as a "list" Type because the grammar is confusing
+	 *	it's context sensitive - need to review.
+	 *  Can't cleanly differentiate Int[3] (List of Int with val '3') and var[3] : int.get[3]
+     *  So for now I'm gonna get a List in the ast and deal with it there
+     *  indexExpr 	:	({notAfterEol()}? sq_bracketL expr sq_bracketR)
+	 * 	-> ^(AST_INDEX_EXPR sq_bracketL expr sq_bracketR);
+	 */
+	public Rule indexExpr()
+	{
+		return sequence(noNl(), SQ_BRACKET_L, expr(), SQ_BRACKET_R);
+	}
+
+	public Rule callOp()
+	{
+		return enforcedSequence(PAR_L, args(), PAR_R, optional(closure()));
+	}
+
+	public Rule litteral()
+	{
+		return firstOf(KW_NULL, KW_THIS, KW_SUPER, KW_IT, KW_TRUE, KW_FALSE,
+						strs(), uri(), number(), char_(), namedSuper(),
+						list(), map());
+	}
+
+	public Rule list()
+	{
+		return sequence(
+			optional(sequence(type(), noNl())),
+			SQ_BRACKET_L, listItems(), SQ_BRACKET_R
+			);
+	}
+
+	public Rule listItems()
+	{
+		return firstOf(
+			SP_COMMA,
+			sequence(expr(), zeroOrMore(enforcedSequence(SP_COMMA, expr())))
+			);
+	}
+
+	public Rule map()
+	{
+		return sequence(optional(mapType()), noNl(), SQ_BRACKET_L, mapItems(), SQ_BRACKET_R);
+	}
+
+	public Rule mapItems()
+	{
+		return firstOf(SP_COL,
+			sequence(mapPair(), zeroOrMore(sequence(SP_COMMA, mapPair())))
+			);
+	}
+
+	public Rule mapPair()
+	{
+		return sequence(expr(), SP_COL, expr());
+	}
+
+	// ------------ Litteral items ---------------------------------------------
+	// Todo: review if allows things like : "aa\"bb"
+	public Rule strs()
+	{
+		return firstOf(
+			sequence(QUOTE, zeroOrMore(sequence(testNot(QUOTE), any())), QUOTE),
+			sequence(QUOTES3, zeroOrMore(sequence(testNot(QUOTES3), any())), QUOTES3)
+			);
+	}
+
+	public Rule uri()
+	{
+		return sequence(TICK, zeroOrMore(sequence(testNot(TICK), any())), TICK);
+	}
+
+	public Rule char_()
+	{
+		return firstOf(
+			enforcedSequence(SINGLE_Q, any(), SINGLE_Q),
+			enforcedSequence("\\u", hexDigit(), hexDigit(), hexDigit(), hexDigit())
+			);
+	}
+
+	public Rule hexDigit()
+	{
+		return firstOf(charRange('a', 'f'),
+						charRange('A','F'),
+						digit()
+						);
+	}
+
+	public Rule digit()
+	{
+		return charRange('0','9');
+	}
+
+	public Rule number()
+	{
+		return sequence(
+			optional(OP_MINUS),
+			firstOf(
+				// hex number
+				sequence(firstOf("0x","0X"),
+						oneOrMore(firstOf("_",hexDigit()))),
+				// decimal
+				sequence(digit(),
+						zeroOrMore(sequence(zeroOrMore("_"), digit())),
+						optional(fraction()),
+						optional(exponent())),
+				// fractional
+				sequence(fraction(), optional(exponent()), optional(nbType()))
+			));
+	}
+
+	public Rule fraction()
+	{
+		return sequence(DOT, digit(), zeroOrMore(sequence(zeroOrMore("_"), digit())));
+	}
+
+	public Rule exponent()
+	{
+		return enforcedSequence(charSet("eE"),
+				optional(firstOf(OP_PLUS, OP_MINUS)),
+				digit(),
+				zeroOrMore(sequence(zeroOrMore("_"), digit()))
+			);
+	}
+
+	public Rule nbType()
+	{
+		return firstOf(
+				"day", "hr", "min", "sec", "ms", "ns", //durations
+				"f", "F", "D", "d" // float / decimal
+			);
 	}
 
 	// ------------ Type -------------------------------------------------------
@@ -404,6 +771,7 @@ public class FantomParser extends BaseParser<Object>
 	public final Rule KW_USING = terminal("using");
 	public final Rule KW_ABSTRACT = terminal("abstract");
 	public final Rule KW_AS = terminal("as");
+	public final Rule KW_ASSERT = terminal("assert"); // not a grammar kw
 	public final Rule KW_BREAK = terminal("break");
 	public final Rule KW_CATCH = terminal("catch");
 	public final Rule KW_CASE = terminal("case");
@@ -411,15 +779,22 @@ public class FantomParser extends BaseParser<Object>
 	public final Rule KW_CONST = terminal("const");
 	public final Rule KW_CONTINUE = terminal("continue");
 	public final Rule KW_DEFAULT = terminal("default");
+	public final Rule KW_DO = terminal("do"); // unused, reserved
 	public final Rule KW_ELSE = terminal("else");
+	public final Rule KW_FALSE = terminal("false");
 	public final Rule KW_FINAL = terminal("final");
 	public final Rule KW_FINALLY = terminal("finally");
 	public final Rule KW_FOR = terminal("for");
+	public final Rule KW_FOREACH = terminal("foreach"); // unused, reserved
 	public final Rule KW_IF = terminal("if");
 	public final Rule KW_INTERNAL = terminal("internal");
+	public final Rule KW_IS = terminal("is");
+	public final Rule KW_IT = terminal("ii");
+	public final Rule KW_ISNOT = terminal("isnot");
 	public final Rule KW_MIXIN = terminal("mixin");
 	public final Rule KW_NATIVE = terminal("native");
 	public final Rule KW_NEW = terminal("new");
+	public final Rule KW_NULL = terminal("null");
 	public final Rule KW_ONCE = terminal("once");
 	public final Rule KW_OVERRIDE = terminal("override");
 	public final Rule KW_PRIVATE = terminal("private");
@@ -432,15 +807,39 @@ public class FantomParser extends BaseParser<Object>
 	public final Rule KW_SWITCH = terminal("switch");
 	public final Rule KW_THIS = terminal("this");
 	public final Rule KW_THROW = terminal("throw");
+	public final Rule KW_TRUE = terminal("true");
 	public final Rule KW_TRY = terminal("tty");
 	public final Rule KW_VIRTUAL = terminal("virtual");
+	public final Rule KW_VOID = terminal("void"); // unused, reserved
+	public final Rule KW_VOLATILE = terminal("volatile"); // unused, reserved
 	public final Rule KW_WHILE = terminal("while");
 	// Non keyword meningful items
 	public final Rule ENUM = terminal("enum");
 	public final Rule FACET = terminal("facet");
 	// operators
+	public final Rule OP_SAFE_CALL = terminal("?.");
+	public final Rule OP_SAFE_DYN_CALL = terminal("?->");
 	public final Rule OP_ARROW = terminal("->");
 	public final Rule OP_ASSIGN = terminal(":=");
+	public final Rule OP_ELVIS = terminal("?:");
+	public final Rule OP_OR = terminal("||");
+	public final Rule OP_AND = terminal("&&");
+	public final Rule OP_RANGE = terminal("..");
+	public final Rule OP_RANGE_EXCL = terminal("..<");
+	public final Rule OP_CURRY = terminal("&");
+	public final Rule OP_BANG = terminal("!");
+	public final Rule OP_2PLUS = terminal("++");
+	public final Rule OP_2MINUS = terminal("--");
+	public final Rule OP_PLUS = terminal("+");
+	public final Rule OP_MINUS = terminal("-");
+	public final Rule OP_MULT = terminal("*");
+	public final Rule OP_DIV = terminal("/");
+	public final Rule OP_POUND = terminal("#");
+	// comparators
+	public final Rule CP_EQUALITY = firstOf(terminal("==="), terminal("!=="),
+							terminal("=="), terminal("!="));
+	public final Rule CP_COMPARATORS = firstOf(terminal("<="), terminal(">="),
+							terminal("<=>"), terminal("<"), terminal(">"));
 	// separators
 	public final Rule SP_PIPE = terminal("|");
 	public final Rule SP_QMARK = terminal("?");
@@ -450,9 +849,17 @@ public class FantomParser extends BaseParser<Object>
 	public final Rule SP_SEMI = terminal(";");
 	// assignment
 	public final Rule AS_EQUAL = terminal("=");
+	public final Rule AS_ASSIGN_OPS = firstOf(terminal("*="),terminal("/="),
+							terminal("%="),terminal("+="),terminal("-="));
 	// others
+	public final Rule QUOTES3 = terminal("\"\"\"");
+	public final Rule QUOTE = terminal("\"");
+	public final Rule TICK = terminal("`");
+	public final Rule SINGLE_Q = terminal("'");
 	public final Rule DOT = terminal(".");
 	public final Rule AT = terminal("@");
+	public final Rule DSL_OPEN = terminal("<|");
+	public final Rule DSL_CLOSE = terminal("|>");
 	public final Rule SQ_BRACKET_L = terminal("[");
 	public final Rule SQ_BRACKET_R = terminal("]");
 	public final Rule BRACKET_L = terminal("{");
