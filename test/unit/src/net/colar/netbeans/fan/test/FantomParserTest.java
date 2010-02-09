25f5b25114c6a66b44e3fb85a4dbaa6b1a7b8adf
tcolar <devnull@localhost>
2010-02-09 23:26:28 +0000

    Parser work More unit test

--- test/unit/src/net/colar/netbeans/fan/test/FantomParserTest.java
+++ test/unit/src/net/colar/netbeans/fan/test/FantomParserTest.java
@@ -3,8 +3,6 @@
  */
 package net.colar.netbeans.fan.test;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import net.colar.netbeans.fan.parboiled.FantomParser;
 import net.jot.testing.JOTTestable;
 import net.jot.testing.JOTTester;
@@ -24,7 +22,170 @@ public class FantomParserTest implements JOTTestable
 	{
 		FantomParser parser = Parboiled.createParser(FantomParser.class);
 
-		DataInputStream dis = new DataInputStream(new FileInputStream("/home/thibautc/NetBeansProjects/Fan/test/parser_test/test.fan"));
+		ParsingResult<Object> result = null;
+		//spacing
+		result = parser.parse(parser.spacing(), "  \r \n\r\t	\n \u000c ");
+		testNodeName("Spacing", result, "spacing");
+		result = parser.parse(parser.spacing(), "   \n//fdsfdsfs\n	\t");
+		testNodeName("Spacing2", result, "spacing");
+		result = parser.parse(parser.spacing(), "   \n/*fdsfdsfs*/	\t");
+		testNodeName("Spacing3", result, "spacing");
+		result = parser.parse(parser.spacing(), "/* blah\nblu"); // unterminated ml comment
+		testNodeName("Spacing4", result, "spacing", "/* blah\n"); // should match until \n
+		// litterals
+		result = parser.parse(parser.char_(), "'a'");
+		testNodeName("Char", result, "char_", "'a'");
+		result = parser.parse(parser.char_(), "'\\n'");
+		testNodeName("Char2", result, "char_");
+		result = parser.parse(parser.char_(), "'\\u5F9a'");
+		testNodeName("Char3", result, "char_");
+		result = parser.parse(parser.uri(), "`http://www.google.com/`");
+		testNodeName("Uri", result, "uri");
+		result = parser.parse(parser.strs(), "\"aa\"");
+		testNodeName("String", result, "strs");
+		result = parser.parse(parser.strs(), "\"aa\\r\\nbb\"");
+		testNodeName("String 2", result, "strs", "\"aa\\r\\nbb\"");
+		result = parser.parse(parser.strs(), "\"a \\n $toto \\$thingy \\t \\u5F39\"");
+		testNodeName("String 3", result, "strs");
+		result = parser.parse(parser.strs(), "\"\"\"a\"\"\"");
+		testNodeName("3Quotes Str 1", result, "strs", "\"\"\"a\"\"\"");
+		result = parser.parse(parser.strs(), "\"\"\"\"hello\" \"\"\"");
+		testNodeName("3Quotes Str 2", result, "strs", "\"\"\"\"hello\" \"\"\"");
+		result = parser.parse(parser.strs(), "\"\"\"\"hello\"\\n\"\" \"\"\"");
+		testNodeName("3Quotes Str 3", result, "strs", "\"\"\"\"hello\"\\n\"\" \"\"\"");
+		result = parser.parse(parser.dsl(), "Str<| dfdsfsd \\ \\n |  > dsfsd |>");
+		testNodeName("DSL", result, "dsl",  "Str<| dfdsfsd \\ \\n |  > dsfsd |>");
+		// numbers
+		result = parser.parse(parser.digit(), "7");
+		testNodeName("Digit", result, "digit");
+		result = parser.parse(parser.hexDigit(), "7");
+		testNodeName("HexDigit", result, "hexDigit");
+		result = parser.parse(parser.hexDigit(), "D");
+		testNodeName("HexDigit2", result, "hexDigit");
+		result = parser.parse(parser.hexDigit(), "d");
+		testNodeName("HexDigit3", result, "hexDigit");
+		result = parser.parse(parser.number(), "0X_5__A6_F");
+		testNodeName("Number 1", result, "number");
+		result = parser.parse(parser.number(), "0xCAFE_BABE");
+		testNodeName("Number 2", result, "number");
+		result = parser.parse(parser.number(), "1_000_000");
+		testNodeName("Number 3", result, "number");
+		result = parser.parse(parser.number(), "-89_039");
+		testNodeName("Number 4", result, "number");
+		result = parser.parse(parser.number(), "-.25");
+		testNodeName("Number f 1", result, "number");
+		result = parser.parse(parser.number(), "3.0f");
+		testNodeName("Number f 2", result, "number");
+		result = parser.parse(parser.number(), "3f");
+		testNodeName("Number f 3", result, "number");
+		result = parser.parse(parser.number(), "123_456.0f");
+		testNodeName("Number f 4", result, "number");
+		result = parser.parse(parser.number(), "3e6f");
+		testNodeName("Number f 5", result, "number");
+		result = parser.parse(parser.number(), "0.2e+6f");
+		testNodeName("Number f 6", result, "number");
+		result = parser.parse(parser.number(), "1_2.3_7e-5_6f");
+		testNodeName("Number f 7", result, "number");
+		result = parser.parse(parser.number(), "4d");
+		testNodeName("Number d 1", result, "number");
+		result = parser.parse(parser.number(), "4.00");
+		testNodeName("Number d 2", result, "number");
+		result = parser.parse(parser.number(), "123_456d");
+		testNodeName("Number d 3", result, "number");
+		result = parser.parse(parser.number(), "0.2e+6D");
+		testNodeName("Number d 4", result, "number");
+		result = parser.parse(parser.number(), "1_2.3_7e-5_6");
+		testNodeName("Number d 5", result, "number");
+		// base items
+		result = parser.parse(parser.doc(), "**\n");
+		testNodeName("Doc", result, "doc");
+		result = parser.parse(parser.doc(), "**\n** some doc\n");
+		testNodeName("Multi Doc", result, "doc");
+		result = parser.parse(parser.id(), "ab_FG09__");
+		testNodeName("ID", result, "id");
+		// types
+		result = parser.parse(parser.typeAndOrId(), "ab_FG09__");
+		testNodeName("TypeAndOrId", result, "typeAndOrId");
+		result = parser.parse(parser.typeAndOrId(), "ab_FG09__ ab_FG09__");
+		testNodeName("TypeAndOrId 2", result, "typeAndOrId", "ab_FG09__ ab_FG09__");
+		result = parser.parse(parser.simpleType(), "myThingy");
+		testNodeName("SimpleType", result, "simpleType");
+		result = parser.parse(parser.simpleType(), "Email::email");
+		testNodeName("SimpleType2", result, "simpleType");
+		result = parser.parse(parser.mapType(), "[Sys::Str:Int]");
+		testNodeName("MapType2", result, "mapType");
+		result = parser.parse(parser.simpleMapType(), "Str:Int");
+		testNodeName("MapType3", result, "simpleMapType");
+		result = parser.parse(parser.simpleMapType(), "[Str:Int]:Str");
+		testNodeName("MapType4", result, "simpleMapType");
+		result = parser.parse(parser.mapType(), "[Str?[]?:Int?]");
+		testNodeName("MapType5", result, "mapType");
+		result = parser.parse(parser.mapType(), "[|Str->Int|:Int?]");
+		testNodeName("MapType6", result, "mapType");
+		result = parser.parse(parser.mapType(), "[Sys::Str:Sys::Int]");
+		testNodeName("MapType7", result, "mapType");
+		result = parser.parse(parser.formals(), "myThingy, Str myOtherThingy, [Str:Int] one5More");
+		testNodeName("Formals", result, "formals");
+		result = parser.parse(parser.funcType(), "|->|");
+		testNodeName("FuncType1", result, "funcType");
+		result = parser.parse(parser.funcType(), "|Sys::Str->|");
+		testNodeName("FuncType2", result, "funcType");
+		result = parser.parse(parser.funcType(), "|->Int|");
+		testNodeName("FuncType3", result, "funcType");
+		result = parser.parse(parser.funcType(), "|Str?[]? s, Int[]? i->[Str:Int?]?|");
+		testNodeName("FuncType4", result, "funcType");
+		result = parser.parse(parser.typeList(), "myThingy");
+		testNodeName("TypeList", result, "typeList");
+		result = parser.parse(parser.typeList(), "myThingy, myOtherThingy, one5More");
+		testNodeName("TypeList2", result, "typeList");
+		result = parser.parse(parser.type(), "toto");
+		testNodeName("Type", result, "type");
+		result = parser.parse(parser.type(), "toto?");
+		testNodeName("Type2", result, "type");
+		result = parser.parse(parser.type(), "toto[]?");
+		testNodeName("Type3", result, "type");
+		result = parser.parse(parser.type(), "Str:email");
+		testNodeName("Type4", result, "type");
+		result = parser.parse(parser.type(), "[Str?[]?:Int]");
+		testNodeName("Type5", result, "type");
+		result = parser.parse(parser.type(), "|Str s, Int->Int|");
+		testNodeName("Type6", result, "type");
+		result = parser.parse(parser.type(), "Sys::Str?[]?");
+		testNodeName("Type7", result, "type");
+
+		// expressions
+		result = parser.parse(parser.mapPair(), "doThis:doThat");
+		testNodeName("Map pair", result, "mapPair", "doThis:doThat");
+		result = parser.parse(parser.mapItems(), "doThis:doThat, a:b, c:d");
+		testNodeName("Map items", result, "mapItems", "doThis:doThat, a:b, c:d");
+		result = parser.parse(parser.map(), "[:]");
+		testNodeName("Map 1", result, "map", "[:]");
+		result = parser.parse(parser.map(), "[5:9]");
+		testNodeName("Map 2", result, "map", "[5:9]");
+		result = parser.parse(parser.map(), "[5:9,7:12]");
+		testNodeName("Map 3", result, "map", "[5:9,7:12]");
+		result = parser.parse(parser.map(), "Int:Str[5:9,7:12]");
+		testNodeName("Map 4", result, "map", "Int:Str[5:9,7:12]");
+		result = parser.parse(parser.map(), "[Int:Str][5:9,7:12]");
+		testNodeName("Map 5", result, "map", "[Int:Str][5:9,7:12]");
+		result = parser.parse(parser.closure(), "|->|{}");
+		testNodeName("Closure 1", result, "closure", "|->|{}");
+		result = parser.parse(parser.closure(), "|->|{i:=5}");
+		testNodeName("Closure 2", result, "closure", "|->|{i:=5}");
+		result = parser.parse(parser.call(), "toto()");
+		testNodeName("Call1", result, "call", "toto()");
+		result = parser.parse(parser.call(), "toto(a, b)");
+		testNodeName("Call2", result, "call", "toto(a, b)");
+		result = parser.parse(parser.call(), "toto|->|{i:=5}");
+		testNodeName("Call3", result, "call", "toto|->|{i:=5}");
+		result = parser.parse(parser.call(), "toto(a, b)|->|{i:=5}");
+		testNodeName("Call4", result, "call", "toto(a, b)|->|{i:=5}");
+
+		// TODO: eos() noNL() ?
+		
+
+
+		/*DataInputStream dis = new DataInputStream(new FileInputStream("/home/thibautc/NetBeansProjects/Fan/test/parser_test/test.fan"));
 		byte[] buffer = new byte[dis.available()];
 		dis.readFully(buffer);
 		String testInput = new String(buffer);
@@ -36,16 +197,40 @@ public class FantomParserTest implements JOTTestable
 
 		if (result.hasErrors())
 		{
+		System.out.println(StringUtils.join(result.parseErrors, "---\n"));
+		}
+		JOTTester.checkIf("Errors? ", ! result.hasErrors());*/
+	}
+
+	public void testNodeName(String label, ParsingResult<Object> result, String nodeName) throws Exception
+	{
+		testNodeName(label, result, nodeName, null);
+	}
+
+	public void testNodeName(String label, ParsingResult<Object> result, String nodeName, String value) throws Exception
+	{
+		if (result.hasErrors())
+		{
 			System.out.println(StringUtils.join(result.parseErrors, "---\n"));
 		}
-		JOTTester.checkIf("Errors? ", ! result.hasErrors());
+		JOTTester.checkIf(label + " - parsing success", !result.hasErrors());
+		JOTTester.checkIf(label + " - root not null", result.parseTreeRoot != null);
+		JOTTester.checkIf(label + " - check name(" + nodeName + ")", result.parseTreeRoot.getLabel().equals(nodeName), result.parseTreeRoot.getLabel());
+		if(value!=null)
+		{
+			String txt = ParseTreeUtils.getNodeText(result.parseTreeRoot, result.inputBuffer);
+			JOTTester.checkIf(label+" - check value", txt.equals(value), "'"+value+"' VS '"+txt+"'");
+		}
 	}
 
 	public static void main(String[] args)
 	{
 		try
 		{
-			new FantomParserTest().jotTest();
-		}catch(Throwable t){t.printStackTrace();}
+			JOTTester.singleTest(new FantomParserTest(), false);
+		} catch (Throwable t)
+		{
+			t.printStackTrace();
+		}
 	}
 }
