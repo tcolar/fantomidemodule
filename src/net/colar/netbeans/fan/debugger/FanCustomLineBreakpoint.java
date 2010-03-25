4e2d94f4fc8126c7e80ed04fcf06f75610e72dc3
tcolar <devnull@localhost>
2010-03-25 23:50:32 +0000

    type resolution fixes (functions, list expressions) Some debugger fixes - still have to fix source resolution

---
+++ src/net/colar/netbeans/fan/debugger/FanCustomLineBreakpoint.java
@@ -0,0 +1,14 @@
+/*
+ * Thibaut Colar Mar 25, 2010
+ */
+
+package net.colar.netbeans.fan.debugger;
+
+/**
+ *
+ * @author thibautc
+ */
+public class FanCustomLineBreakpoint extends LineBreakpointImpl
+{
+
+}
