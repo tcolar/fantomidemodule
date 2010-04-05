e05f7111864fccb593e24d93c11ace77c0cde23e
bheadley <devnull@localhost>
2010-04-05 23:28:03 +0000

    took out unused imports

--- src/net/colar/netbeans/fan/types/FanResolvedNullType.java
+++ src/net/colar/netbeans/fan/types/FanResolvedNullType.java
@@ -4,9 +4,7 @@
 
 package net.colar.netbeans.fan.types;
 
-import net.colar.netbeans.fan.indexer.model.FanType;
 import net.colar.netbeans.fan.parboiled.AstNode;
-import net.jot.persistance.JOTTransaction;
 
 /**
  * Special type to carry "null"
