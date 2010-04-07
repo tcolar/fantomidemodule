e8bde0743f3dc95a6bc3713abb415c5c0f3c2857
tcolar <devnull@localhost>
2010-04-07 07:20:50 +0000

    test prj

---
+++ test/ideTest/java/JavaClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package java;
+
+/**
+ *
+ * @author tcolar
+ */
+public class JavaClass {
+
+}
