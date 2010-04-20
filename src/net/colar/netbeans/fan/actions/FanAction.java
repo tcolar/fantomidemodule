d4f2270c949b05a274e5ef04008620762f241e27
tcolar <devnull@localhost>
2010-04-20 04:53:39 +0000

    Fix to node action project finder (build/debug etc..) such as it's smarter at finding the right project to run the task against.

--- src/net/colar/netbeans/fan/actions/FanAction.java
+++ src/net/colar/netbeans/fan/actions/FanAction.java
@@ -6,6 +6,7 @@ package net.colar.netbeans.fan.actions;
 
 import java.util.concurrent.Future;
 import javax.swing.JOptionPane;
+import net.colar.netbeans.fan.FanUtilities;
 import net.colar.netbeans.fan.platform.FanPlatform;
 import net.colar.netbeans.fan.project.FanProject;
 import net.colar.netbeans.fan.project.FanProjectProperties;
@@ -98,13 +99,13 @@ public abstract class FanAction
 			String path = FileUtil.toFile(file.getParent()).getAbsolutePath();
 			// see if user specified custom main method
 			String target = podName + "::" + FanProjectProperties.getProperties(project).getMainMethod();
-			if (target == null || target.length()==0)
+			if (target == null || target.length() == 0)
 			{
 				// otherwise use default
 				target = podName + "::" + "Main" + "." + "main";
 			}
 			FanExecution fanExec = new FanExecution();
-			fanExec.setDisplayName((debug?"Debug ":"")+file.getName());
+			fanExec.setDisplayName((debug ? "Debug " : "") + file.getName());
 			fanExec.setWorkingDirectory(path);
 			FanPlatform.getInstance().buildFanCall(fanExec, debug);
 			fanExec.addCommandArg(FanPlatform.FAN_CLASS);
@@ -122,34 +123,33 @@ public abstract class FanAction
 	 */
 	/*private Future buildActionExternal(Lookup lookup, String target)
 	{
-		// if default target "", see what user chose in props;
-		if (target.equals(""))
-		{
-			String newTarget = FanProjectProperties.getProperties(project).getBuildTarget();
-			if (newTarget != null)
-			{
-				target = newTarget;
-			}
-		}
-		FileObject file = findTargetProject(lookup);
-		if (file != null)
-		{
-			FileObject buildFile = file.getFileObject("build.fan");
-			if (buildFile != null)
-			{
-				String path = FileUtil.toFile(file).getAbsolutePath();
-				FanExecution fanExec = new FanExecution();
-				fanExec.setDisplayName(file.getName());
-				fanExec.setWorkingDirectory(path);
-				fanExec.setCommand(FanPlatform.getInstance().getFanBinaryPath());
-				fanExec.addCommandArg("build.fan");
-				fanExec.addCommandArg(target);
-				return fanExec.run();
-			}
-		}
-		return null;
+	// if default target "", see what user chose in props;
+	if (target.equals(""))
+	{
+	String newTarget = FanProjectProperties.getProperties(project).getBuildTarget();
+	if (newTarget != null)
+	{
+	target = newTarget;
+	}
+	}
+	FileObject file = findTargetProject(lookup);
+	if (file != null)
+	{
+	FileObject buildFile = file.getFileObject("build.fan");
+	if (buildFile != null)
+	{
+	String path = FileUtil.toFile(file).getAbsolutePath();
+	FanExecution fanExec = new FanExecution();
+	fanExec.setDisplayName(file.getName());
+	fanExec.setWorkingDirectory(path);
+	fanExec.setCommand(FanPlatform.getInstance().getFanBinaryPath());
+	fanExec.addCommandArg("build.fan");
+	fanExec.addCommandArg(target);
+	return fanExec.run();
+	}
+	}
+	return null;
 	}*/
-
 	/**
 	 * Call the action using internal call to Fan java class (not the wrapper scripts).
 	 * @param lookup
@@ -205,17 +205,22 @@ public abstract class FanAction
 			if (gdo != null && gdo.getPrimaryFile() != null)
 			{
 				file = gdo.getPrimaryFile();
+				if (FanProject.isProject(file))
+				{
+					return file;
+				}
 			}
 		}
-		if (file == null)
+		// If we get there, current selected item is NOT a project
+		// so use the "main" one
+		// use "main project", if fan project
+		Project prj = OpenProjects.getDefault().getMainProject();
+		if (prj != null && FanProject.isProject(prj.getProjectDirectory()))
 		{
-			// use "main project", if fan project
-			Project prj = OpenProjects.getDefault().getMainProject();
-			if (prj!=null && FanProject.isProject(prj.getProjectDirectory()))
-			{
-				file = OpenProjects.getDefault().getMainProject().getProjectDirectory();
-			}
+			return OpenProjects.getDefault().getMainProject().getProjectDirectory();
 		}
-		return file;
+		// try to fall back to the selected item project folder
+		return file==null?null
+				: FileUtil.toFileObject(FanUtilities.getPodFolderForPath(file.getPath()));
 	}
 }
